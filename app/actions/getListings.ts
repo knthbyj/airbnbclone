906f8e6a4cf825c4a3202c5203f667202fb4050a
Kenneth <kennethbyj@gmail.com>
2023-06-05 02:20:56 +0800

    Added logic for filters for categories rooms guests bathrooms location

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,19 +2,79 @@ import prisma from "@/app/lib/prismadb"
 
 export interface IListingsParams {
     userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
 }
 
 export default async function getListings(
     params:IListingsParams
 ) {
     try {
-        const { userId } = params;
+        const { 
+            userId,
+            guestCount,
+            roomCount,
+            bathroomCount,
+            locationValue,
+            startDate,
+            endDate,
+            category,
+        } = params;
 
         let query: any = {};
 
         if (userId) {
             query.userId = userId;
         }
+
+        if (category) {
+            query.category = category;
+        }
+
+        if (roomCount) {
+            query.roomCount = {
+                gte: +roomCount//adding plus sign in front converts string to number format
+            }
+        }
+        if (bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount//adding plus sign in front converts string to number format
+            }
+        }
+        if (guestCount) {
+            query.guestCount = {
+                gte: +guestCount//adding plus sign in front converts string to number format
+            }
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue;
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: { gte: startDate },
+                                startDate: { lte: startDate },
+                            },
+                            {
+                                startDate: { lte: endDate },
+                                endDate: { gte: endDate },
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+
         const listings = await prisma.listing.findMany({
             where: query,
             orderBy: {
